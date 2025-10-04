114ddd0ef65365d934a194e0c5b80f4249b2af75
althaf <shaikalthaf1984@gmail.com>
2025-10-04 15:56:21 +0530

    25-disk-usage.sh

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -2,13 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-    echo "High Usage on $PARTITION: $USAGE"
+    MESSAGE="High Disk Usage on $PARTITION: $USAGE %"
     fi
 
 done <<< $DISK_USAGE
+
+    echo -e "Message Body: $MESSAGE"
