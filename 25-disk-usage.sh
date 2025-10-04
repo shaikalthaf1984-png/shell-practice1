b3c1bbaa4845ef00637379dc5a97def73429929e
althaf <shaikalthaf1984@gmail.com>
2025-10-04 15:41:29 +0530

    25-disk-usage.sh

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2    
 
 while IFS= read -r line
 do
-echo "Line: $line"
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD]; then
+    echo "High Usage on $PARTITION: $USAGE"
+    fi
 
 done <<< $DISK_USAGE
