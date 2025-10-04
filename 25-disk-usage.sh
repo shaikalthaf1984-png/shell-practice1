aaf65f4305b94f33a19c24f584137a2b3721b64e
althaf <shaikalthaf1984@gmail.com>
2025-10-04 17:51:12 +0530

    25-disk-usage.sh

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=2
+DISK_THRESHOLD=2 # in project we keep it as 75
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 
@@ -10,20 +10,16 @@ do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-    MESSAGE+="High Disk Usage on $PARTITION: $USAGE % \n"
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>" # escaping
     fi
-
 done <<< $DISK_USAGE
 
-    echo -e "Message Body: $MESSAGE"
+echo -e "Message Body: $MESSAGE"
 
-    sh mail.sh "shaikalthaf1984@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRRESS" "Devops Team"
+sh mail.sh "shaikalthaf1984@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
 
-    #TO_ADDRESS=$1
-    #SUBJECT=$2
-    #ALERT_TYPE=$3
-    #MESSAGE_BODY=$4
-    #IP_ADDRESS=$5
-    #TO_TEAM=$6
-
-
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
