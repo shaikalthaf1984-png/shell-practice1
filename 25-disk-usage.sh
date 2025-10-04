bed1e5bfb0787ee19dc48174cc4702283ec24c90
althaf <shaikalthaf1984@gmail.com>
2025-10-04 15:43:22 +0530

    25-disk-usage.sh

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -7,7 +7,7 @@ while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
-    if [ $USAGE -ge $DISK_THRESHOLD]; then
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
     echo "High Usage on $PARTITION: $USAGE"
     fi
 
