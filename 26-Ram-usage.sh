3fd943474b529a1cd7f8b3eae0a3ed3ac05bd658
althaf <shaikalthaf1984@gmail.com>
2025-10-05 09:58:24 +0530

    26-Ram-usage.sh

--- 26-Ram-usage.sh
+++ 26-Ram-usage.sh
@@ -33,3 +33,4 @@ if (( $(echo "$used_percentage > $THRESHOLD" | bc -l) )); then
   # echo "High RAM usage" | mail -s "RAM Alert on server" your_email@example.com
 else
   echo "RAM usage is within acceptable limits"
+  fi
