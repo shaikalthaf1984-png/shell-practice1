668e34b3904282892de0dae380ed7ef5db2c00f6
althaf <shaikalthaf1984@gmail.com>
2025-10-05 12:11:54 +0530

    26-Ram-usage.sh

--- 26-Ram-usage.sh
+++ 26-Ram-usage.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-MEM_INFO=$(free -m | awk 'NR==2')
-TOTAL_MEM=$(echo "$MEM_INFO" | awk '{print $2}')
-USED_MEM=$(echo "$MEM_INFO" | awk '{print $3}')
+# Get total and used memory from free command
+# The `free -m` command shows memory in megabytes
+# The `awk` command is used to process the second line (Mem:)
+# $2 is the total memory, $3 is the used memory
+MEM_INFO=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+RAM_THRESHOLD=2
+TOTAL_MEM=$(free -m | grep  Mem | awk '{print $2}')
+USED_MEM=$(free -m | grep Mem | awk '{print $3}' )
+AVAILABLE_MEM=$(free -m | grep Mem | awk '{print $7}')
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 
-if [ "$TOTAL_MEM" -gt 0 ]; then
-  MEM_PERCENTAGE=$(awk "BEGIN {printf \"%.2f\", ($USED_MEM / $TOTAL_MEM) * 100}")
-  echo "Memory Usage: $USED_MEM MB / $TOTAL_MEM MB ($MEM_PERCENTAGE%)"
-else
-  echo "Error: Total memory is zero."
-fi
+while IFS= read -r line
+do
+USAGE=$( $USED_MEM / $TOTAL_MEM * 100 )
+CURRE_AVAILABLE=$(echo $USAGE | awk '{print $7}')
+if [ $USAGE -ge $RAM_THRESHOLD ]; then
+MESSAGE+="High RAM Usage on $CURRE_AVAILABLE: $USAGE % <br>"
+ fi
+done <<< $MEM_INFO
+
+echo -e "Message Body: $MESSAGE"
+
+sh mail.sh "shaikalthaf1984@gmail.com" "High RAM Usage Alert" "High RAM Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
+
