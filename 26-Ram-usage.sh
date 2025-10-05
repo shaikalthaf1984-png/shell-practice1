0838e0a62770499f78d0d83ecfeef84b286ea8aa
althaf <shaikalthaf1984@gmail.com>
2025-10-05 09:25:57 +0530

    25-disk-usage.sh

---
+++ 26-Ram-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MEM_USAGE=$(free -h | grep  Mem)
+THRESHOLD=2
+
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk '{print $2}')
+    MEMORY=$(echo $line | awk '{print $3}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High Ram usage on $Memory: $USAGE % <br>" # escaping
+    fi
+done <<< $MEM_USAGE
