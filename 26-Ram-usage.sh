11e72601784b06dc0ab07bc6076170f0eedeccba
althaf <shaikalthaf1984@gmail.com>
2025-10-05 09:36:58 +0530

    26-Ram-usage.sh

--- 26-Ram-usage.sh
+++ 26-Ram-usage.sh
@@ -8,7 +8,7 @@ MESSAGE=""
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print $2}')
+    USAGE=$(echo $line | awk '{print $2}' | cut -d "" -f1)
     MEMORY=$(echo $line | awk '{print $3}')
     if [ $USAGE -ge $RAM_THRESHOLD ]; then
         MESSAGE+="High Ram usage on $Memory: $USAGE % <br>" # escaping
