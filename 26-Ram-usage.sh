e1649eb28511010468e6ab7255672f9af7832fec
althaf <shaikalthaf1984@gmail.com>
2025-10-05 10:32:51 +0530

    26-Ram-usage.sh

--- 26-Ram-usage.sh
+++ 26-Ram-usage.sh
@@ -4,7 +4,7 @@
 # The `free -m` command shows memory in megabytes
 # The `awk` command is used to process the second line (Mem:)
 # $2 is the total memory, $3 is the used memory
-MEM_USAGE=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+MEM_USAGE=$(free -m | awk 'NR==2')
 RAM_THRESHOLD=2
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
