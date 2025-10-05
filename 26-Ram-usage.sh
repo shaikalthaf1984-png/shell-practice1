913935786c7e91d7cf9f0bf0deb51016d88f654f
althaf <shaikalthaf1984@gmail.com>
2025-10-05 11:08:40 +0530

    26-Ram-usage.sh

--- 26-Ram-usage.sh
+++ 26-Ram-usage.sh
@@ -4,13 +4,30 @@
 # The `free -m` command shows memory in megabytes
 # The `awk` command is used to process the second line (Mem:)
 # $2 is the total memory, $3 is the used memory
-MEM_USAGE=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+MEM_INFO=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
 RAM_THRESHOLD=2
+TOTAL_MEM=$(free -m | grep  Mem | awk '{print $2}')
+USED_MEM=$(free -m | grep Mem | awk '{print $3}' )
+AVAILABLE_MEM=$(free -m | grep Mem | awk '{print $7}')
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 
 while IFS=read -r line
 do
-USAGE=$(echo $line | awk '{print $3}')
-AVAILABLE=$(echo $line | awk '{print $7}')
+USAGE=$(echo $AVAILABLE_MEM | awk '{print $3}')
+CURRE_AVAILABLE=$(echo $AVAILABLE_MEM -ge 2 | awk '{print $7}')
+if [ $USAGE -ge $RAM_THRESHOLD ]; then
+MESSAGE+="High RAM Usage on $CURRE_AVAILABLE: $USAGE % <br>"
+ fi
+done <<< $MEM_INFO
+
+echo -e "Message Body: $MESSAGE"
+
+#sh mail.sh "shaikalthaf1984@gmail.com" "High RAM Usage Alert" "High RAM Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
 
